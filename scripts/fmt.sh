093051302fa292e56ce8142e2810ce85e164dc37
Mahmoud Abumandour <mam47@sfu.ca>
2024-12-31 13:45:10 -0800

    fmt: Move to root dir before executing

--- scripts/fmt.sh
+++ scripts/fmt.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+GIT_REPO_ROOT=$(git rev-parse --show-toplevel)
+cd $GIT_REPO_ROOT
+
 FILES=$(find * -not -path "build/*" | grep -E "^.*(\.h|\.cpp)$")
 printf "Formatting files:\n$FILES\n"
 echo "$FILES" | xargs clang-format -i
