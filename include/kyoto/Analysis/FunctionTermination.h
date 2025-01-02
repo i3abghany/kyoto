ae3da5505b159553bd854b1e319d26b998ed5b00
Mahmoud Abumandour <mam47@sfu.ca>
2025-01-01 16:43:41 -0800

    Add the FunctionTermination LLVM pass

---
+++ include/kyoto/Analysis/FunctionTermination.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "llvm/IR/Analysis.h"
+#include "llvm/IR/PassManager.h"
+
+class ModuleCompiler;
+
+namespace llvm {
+class Function;
+class BasicBlock;
+}
+
+struct FunctionTerminationPass : public llvm::PassInfoMixin<FunctionTerminationPass> {
+
+    FunctionTerminationPass(ModuleCompiler& compiler);
+
+    llvm::PreservedAnalyses run(llvm::Function& func, llvm::FunctionAnalysisManager& FAM);
+
+    bool is_basic_block_unreachable(llvm::BasicBlock& bb);
+    void verify_function_termination(llvm::Function& func);
+
+private:
+    ModuleCompiler& compiler;
+};
---
+++ src/Analysis/FunctionTermination.cpp
@@ -0,0 +1,59 @@
+#include "kyoto/Analysis/FunctionTermination.h"
+
+#include "kyoto/ModuleCompiler.h"
+#include "llvm/ADT/SmallVector.h"
+#include "llvm/ADT/ilist_iterator.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/CFG.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/Support/Casting.h"
+#include "llvm/Support/raw_ostream.h"
+
+FunctionTerminationPass::FunctionTerminationPass(ModuleCompiler& compiler)
+    : compiler(compiler)
+{
+}
+
+llvm::PreservedAnalyses FunctionTerminationPass::run(llvm::Function& func, llvm::FunctionAnalysisManager& FAM)
+{
+    verify_function_termination(func);
+    return llvm::PreservedAnalyses::all();
+}
+
+bool FunctionTerminationPass::is_basic_block_unreachable(llvm::BasicBlock& bb)
+{
+    if (&bb == &bb.getParent()->getEntryBlock()) {
+        return false;
+    }
+
+    for (auto* pred : llvm::predecessors(&bb)) {
+        if (auto* term = pred->getTerminator(); !llvm::isa<llvm::ReturnInst>(term)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void FunctionTerminationPass::verify_function_termination(llvm::Function& func)
+{
+    bool has_error = false;
+    for (auto& bb : func) {
+        auto* term = bb.getTerminator();
+        if (term && (llvm::isa<llvm::BranchInst>(term) || llvm::isa<llvm::ReturnInst>(term))) {
+            continue;
+        }
+
+        if (is_basic_block_unreachable(bb)) {
+            compiler.insert_dummy_return(bb);
+            continue;
+        }
+        llvm::errs() << "Function " << func.getName() << " does not terminate properly\n";
+        has_error = true;
+    }
+
+    if (has_error) {
+        compiler.set_fn_termination_error();
+    }
+}
