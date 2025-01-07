500745fd907f7eb10d69d4e1b78d41ec8bf89f87
Mahmoud Abumandour <mam47@sfu.ca>
2025-01-06 17:26:57 -0800

    Add function call testing

---
+++ test/TestFunction.cpp
@@ -0,0 +1,27 @@
+#include <gtest/gtest-param-test.h>
+#include <gtest/gtest.h>
+#include <string>
+#include <vector>
+
+#include "kyoto/utils/File.h"
+#include "kyoto/utils/Test.h"
+
+class TestFunction : public ::testing::TestWithParam<utils::TestCase> {
+protected:
+    utils::TestCase test_case;
+
+    void SetUp() override { test_case = GetParam(); }
+};
+
+TEST_P(TestFunction, TestFunction)
+{
+    utils::test_driver(test_case);
+}
+
+static auto generate_test_cases()
+{
+    const auto test_cases = utils::File::get_test_cases("../test/code/func.kyo");
+    return test_cases;
+}
+
+INSTANTIATE_TEST_SUITE_P(TestFunction, TestFunction, testing::ValuesIn(generate_test_cases()));
