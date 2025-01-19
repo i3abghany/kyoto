17678e85071918c4b15dc6d3ad3d40a315224254
Mahmoud Abumandour <mam47@sfu.ca>
2025-01-18 17:04:03 -0800

    Add KType distiguishing utilities

--- include/kyoto/KType.h
+++ include/kyoto/KType.h
@@ -14,7 +14,9 @@ class KType {
 public:
     virtual ~KType() = default;
     virtual std::string to_string() const = 0;
+    virtual bool is_primitive() const { return false; }
     virtual bool is_pointer() const { return false; }
+    virtual bool is_class() const { return false; }
     virtual bool is_void() const { return false; }
     virtual bool is_string() const { return false; }
     virtual bool is_integer() const { return false; }
@@ -35,7 +37,7 @@ public:
     {
         auto* ptr = dynamic_cast<T*>(this);
         if (!ptr) {
-            throw std::runtime_error(fmt::format("Cannot cast {} to {}", to_string(), typeid(T).name()));
+            throw std::runtime_error(fmt::format("KType::as: Cannot cast {} to {}", to_string(), typeid(T).name()));
         }
         return ptr;
     }
@@ -49,6 +51,7 @@ public:
 
     explicit PrimitiveType(Kind kind);
     std::string to_string() const override;
+    [[nodiscard]] bool is_primitive() const override;
     bool operator==(const KType& other) const override;
     KType* copy() const override;
 
@@ -78,8 +81,19 @@ public:
     KType* get_pointee() const;
     bool is_string() const override;
     bool is_pointer() const override;
-    size_t ptr_level() const override;
 
 private:
     KType* pointee;
+};
+
+class ClassType : public KType {
+public:
+    explicit ClassType(std::string name);
+    std::string to_string() const override;
+    [[nodiscard]] bool is_class() const override;
+    bool operator==(const KType& other) const override;
+    KType* copy() const override;
+
+private:
+    std::string name;
 };
