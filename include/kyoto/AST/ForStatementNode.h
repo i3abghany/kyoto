b96ba9074c3472726bd22d2dd6dfbeb614ff438a
Mahmoud Abumandour <ma.mandourr@gmail.com>
2025-01-26 21:52:23 -0800

    Refactor the ForStatementNode::gen implementation

--- include/kyoto/AST/ForStatementNode.h
+++ include/kyoto/AST/ForStatementNode.h
@@ -7,6 +7,10 @@
 class ModuleCompiler;
 class ExpressionNode;
 
+namespace llvm {
+class BasicBlock;
+}
+
 class ForStatementNode : public ASTNode {
 public:
     ForStatementNode(ASTNode* init, ExpressionStatementNode* condition, ExpressionNode* update, ASTNode* body,
@@ -17,6 +21,13 @@ public:
     [[nodiscard]] llvm::Value* gen() override;
 
     [[nodiscard]] std::vector<ASTNode*> get_children() const override;
+
+private:
+    void handle_init() const;
+    void handle_condition(llvm::BasicBlock* cond_bb, llvm::BasicBlock* body_bb, llvm::BasicBlock* out_bb) const;
+    bool handle_trivial_false_condition(llvm::BasicBlock* out_bb) const;
+    void handle_update(llvm::BasicBlock* update_bb, llvm::BasicBlock* cond_bb, llvm::BasicBlock* body_bb) const;
+    void verify_bool_condition() const;
 
 private:
     ASTNode* init;
--- src/AST/ForStatementNode.cpp
+++ src/AST/ForStatementNode.cpp
@@ -42,57 +42,83 @@ llvm::Value* ForStatementNode::gen()
     auto* update_bb = update ? compiler.create_basic_block("for_update") : nullptr;
     auto* out_bb = compiler.create_basic_block("for_out");
 
-    if (init) {
-        auto* init_block = compiler.create_basic_block("for_init");
-        compiler.get_builder().CreateBr(init_block);
-        compiler.get_builder().SetInsertPoint(init_block);
-        init->gen();
-    }
+    handle_init();
+    verify_bool_condition();
 
-    bool trivial_false_cond = false;
-    if (condition && condition->get_expr()->is_trivially_evaluable()) {
-        auto* cond_val = condition->get_expr()->gen();
-        auto* cond_ktype = condition->get_expr()->get_ktype();
+    if (handle_trivial_false_condition(out_bb)) return nullptr;
 
-        if (!cond_ktype->is_boolean()) {
-            throw std::runtime_error(
-                fmt::format("For condition must be of type bool, got {}", cond_ktype->to_string()));
-        }
-
-        // if evaluates to false, set the insert point to the out block
-        if (auto* constant_int = llvm::dyn_cast<llvm::ConstantInt>(cond_val)) {
-            if (constant_int->isZero()) {
-                compiler.get_builder().CreateBr(out_bb);
-                compiler.get_builder().SetInsertPoint(out_bb);
-                trivial_false_cond = true;
-            }
-        }
-    }
-
-    if (trivial_false_cond) return nullptr;
-
-    compiler.get_builder().CreateBr(cond_bb ? cond_bb : body_bb);
-
-    if (condition) {
-        compiler.get_builder().SetInsertPoint(cond_bb);
-        auto* cond_val = condition->gen();
-        compiler.get_builder().CreateCondBr(cond_val, body_bb, out_bb);
-    }
+    handle_condition(cond_bb, body_bb, out_bb);
 
     compiler.get_builder().SetInsertPoint(body_bb);
     body->gen();
 
-    if (update) {
-        compiler.get_builder().CreateBr(update_bb);
-        compiler.get_builder().SetInsertPoint(update_bb);
-        auto* _ = update->gen();
-        compiler.get_builder().CreateBr(cond_bb);
-    } else {
-        compiler.get_builder().CreateBr(cond_bb ? cond_bb : body_bb);
-    }
+    handle_update(update_bb, cond_bb, body_bb);
 
     compiler.get_builder().SetInsertPoint(out_bb);
     return nullptr;
+}
+
+void ForStatementNode::handle_init() const
+{
+    if (!init) return;
+    auto* init_block = compiler.create_basic_block("for_init");
+    compiler.get_builder().CreateBr(init_block);
+    compiler.get_builder().SetInsertPoint(init_block);
+    init->gen();
+}
+
+void ForStatementNode::handle_condition(llvm::BasicBlock* cond_bb, llvm::BasicBlock* body_bb,
+                                        llvm::BasicBlock* out_bb) const
+{
+    compiler.get_builder().CreateBr(cond_bb ? cond_bb : body_bb);
+    if (!condition) return;
+
+    compiler.get_builder().SetInsertPoint(cond_bb);
+    auto* cond_val = condition->gen();
+    compiler.get_builder().CreateCondBr(cond_val, body_bb, out_bb);
+}
+
+void ForStatementNode::verify_bool_condition() const
+{
+    if (!condition) return;
+    if (!condition->get_expr()->get_ktype()->is_boolean()) {
+        throw std::runtime_error(
+            fmt::format("For condition must be of type bool, got {}", condition->get_expr()->get_ktype()->to_string()));
+    }
+}
+
+bool ForStatementNode::handle_trivial_false_condition(llvm::BasicBlock* out_bb) const
+{
+    if (!condition) return false;
+    if (!condition->get_expr()->is_trivially_evaluable()) return false;
+
+    auto* cond_val = condition->get_expr()->gen();
+    auto* cond_ktype = condition->get_expr()->get_ktype();
+
+    // if evaluates to false, set the insert point to the out block
+    if (auto* constant_int = llvm::dyn_cast<llvm::ConstantInt>(cond_val)) {
+        if (constant_int->isZero()) {
+            compiler.get_builder().CreateBr(out_bb);
+            compiler.get_builder().SetInsertPoint(out_bb);
+            return true;
+        }
+    }
+
+    return false;
+}
+
+void ForStatementNode::handle_update(llvm::BasicBlock* update_bb, llvm::BasicBlock* cond_bb,
+                                     llvm::BasicBlock* body_bb) const
+{
+    if (!update) {
+        compiler.get_builder().CreateBr(cond_bb ? cond_bb : body_bb);
+        return;
+    }
+
+    compiler.get_builder().CreateBr(update_bb);
+    compiler.get_builder().SetInsertPoint(update_bb);
+    auto* _ = update->gen();
+    compiler.get_builder().CreateBr(cond_bb);
 }
 
 std::vector<ASTNode*> ForStatementNode::get_children() const
