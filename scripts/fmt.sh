8586cee8ab067e1844104b80829bc4f272ef857b
Mahmoud Abumandour <ma.mandourr@gmail.com>
2025-06-05 22:07:27 -0700

    Format rust files in scripts/fmt.sh

--- scripts/fmt.sh
+++ scripts/fmt.sh
@@ -4,5 +4,14 @@ GIT_REPO_ROOT=$(git rev-parse --show-toplevel)
 cd $GIT_REPO_ROOT
 
 FILES=$(find * -not -path "build/*" -not -path "cmake-build-debug/*" | grep -E "^.*(\.h|\.cpp)$")
-printf "Formatting files:\n$FILES\n"
+printf "Formatting C++ files:\n$FILES\n"
 echo "$FILES" | xargs clang-format -i
+
+RUST_FILES=$(find fuzzer -name "*.rs")
+printf "\nFormatting Rust files:\n$RUST_FILES\n"
+if ! command -v rustfmt &> /dev/null; then
+    echo "rustfmt could not be found."
+    exit 1
+fi
+echo "$RUST_FILES" | xargs rustfmt
+    
